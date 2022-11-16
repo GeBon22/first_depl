ab5bdb7c7b2c15b48b680ac0090984a2fa11bc0d
Georg Bonin <114496506+GeBon22@users.noreply.github.com>
2022-11-16 14:57:41 +0100

    added h2

--- pages/index.js
+++ pages/index.js
@@ -6,6 +6,7 @@ export default function Home() {
   return (
     <div className={styles.container}>
       <h1>Hello world - and hello Ralf!</h1>
+      <h2>This is weird!</h2>
       <p>Keep on fighting...</p>
     </div>
   )
